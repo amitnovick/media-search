fd0aef3a0f947aa35e8817e159ea06089b778d3f
Amit Novick <axnovick@gmail.com>
2019-11-09 23:51:43 +0200

    Snapshot: now with images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import "./App.scss";
 
 class App extends React.Component {
@@ -6,16 +9,25 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      isTopTenActive: false
+      isTopTenActive: false,
+      tracks: {
+        1478348234: {
+          imageUrl:
+            "https://is3-ssl.mzstatic.com/image/thumb/Video123/v4/4f/7e/21/4f7e21a1-f172-1ad8-268a-d3754b664842/source/600x600bb.jpg",
+          trackName: "Circles"
+        }
+      },
+      displayedTrackIds: Array(25).fill(1478348234) // the value 1478348234 is the trackId for Post Malone's Circles track
     };
   }
 
   fetchData = () => {
     const ENDPOINT_URL = "https://itunes.apple.com";
+    console.log("fetching...");
   };
 
   render() {
-    const { isTopTenActive } = this.state;
+    const { isTopTenActive, displayedTrackIds, tracks } = this.state;
     return (
       <div className="background">
         <div className="container">
@@ -36,7 +48,15 @@ class App extends React.Component {
               type="text"
               className="input"
               placeholder="Search iTunes for media..."
+              onKeyDown={({ key }) => {
+                if (key === "Enter") {
+                  this.fetchData();
+                }
+              }}
             />
+            <button className="search-button" onClick={() => this.fetchData()}>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
           </div>
           <div
             className="horizontal-list-box"
@@ -69,9 +89,18 @@ class App extends React.Component {
               <li>twelve</li>
             </ol>
           </div>
-          <button className="search-button" onClick={() => this.fetchData()}>
-            Search
-          </button>
+          <div className="tracks">
+            {displayedTrackIds.map((trackId, index) => (
+              <div className="track-container" key={index}>
+                <img
+                  className="track-image"
+                  src={tracks[trackId].imageUrl}
+                  alt="artwork"
+                />
+                <h2>{tracks[trackId].trackName}</h2>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
